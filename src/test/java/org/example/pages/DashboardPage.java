e473dfe5fd511a7a8c54281d736c287ac8c7b918
Sakya Thilakarathne <91802025+sakya-thilakarathne@users.noreply.github.com>
2025-01-04 00:32:54 +0530

    Implemented PIM.

--- src/test/java/org/example/pages/DashboardPage.java
+++ src/test/java/org/example/pages/DashboardPage.java
@@ -13,4 +13,9 @@ public class DashboardPage {
         page.click("a[href='/web/index.php/recruitment/viewRecruitmentModule']");
         page.waitForSelector("h6:has-text('Recruitment')", new Page.WaitForSelectorOptions().setTimeout(10000));
     }
+
+    public void navigateToPIMModule() {
+        page.click("a[href='/web/index.php/pim/viewPimModule']");
+        page.waitForSelector("h6:has-text('PIM')", new Page.WaitForSelectorOptions().setTimeout(10000));
+    }
 }
